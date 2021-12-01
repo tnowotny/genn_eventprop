4b8077c9bbca8826b5143c1dbbe485f97f8cfe84
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2021-12-01 20:00:40 +0000

    Added more debugging code and ficed output code.

--- debug.py
+++ debug.py
@@ -3,41 +3,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 p["N_TRAIN"]= 10*p["N_BATCH"]
-p["REC_SPIKES"]= ["input"]
-p["REC_NEURONS"]= {"hidden": "lambda_V",
-                   "hidden": "lambda_I",
-                   "output": "lambda_V",
-                   "output": "lambda_I"
-                   }
-p["REC_SYNAPSES"]= {"in_to_hid": "w",
-                    "in_to_hid": "dw",
-                    "hid_to_out": "w",
-                    "hid_to_out": "dw"
-                    }
+p["REC_SPIKES"]= ["input","hidden","output"]
+p["REC_NEURONS"]= [("hidden", "lambda_V"),
+                   ("hidden", "lambda_I"),
+                   ("output", "lambda_V"),
+                   ("output", "lambda_I")
+                   ]
+p["REC_SYNAPSES"]= [("in_to_hid", "w"),
+                    ("in_to_hid", "dw"),
+                    ("hid_to_out", "w"),
+                    ("hid_to_out", "dw")
+                    ]
                     
 spike_t, spike_ID, rec_vars_n, rec_vars_s= run_yingyang(p)
 
 plt.figure()
 plt.scatter(spike_t["input"], spike_ID["input"])
-
+plt.scatter(spike_t["hidden"], spike_ID["hidden"]+6,"r")
+plt.scatter(spike_t["output"], spike_ID["output"]+207,"g")
+print(rec_vars_n)
 plt.figure()
-plt.plot(rec_vars_n["hiddenlambdaV"])
+plt.plot(rec_vars_n["lambda_Vhidden"])
 plt.figure()
-plt.plot(rec_vars_n["hiddenlambdaI"])
+plt.plot(rec_vars_n["lambda_Ihidden"])
 plt.figure()
-plt.plot(rec_vars_n["outputlambdaV"])
+plt.plot(rec_vars_n["lambda_Voutput"])
 plt.figure()
-plt.plot(rec_vars_n["outputlambdaI"])
+plt.plot(rec_vars_n["lambda_Ioutput"])
 plt.figure()
-plt.plot(rec_vars_n["in_to_hidw"])
+plt.plot(rec_vars_s["win_to_hid"])
 plt.figure()
-plt.plot(rec_vars_n["in_to_hiddw"])
+plt.plot(rec_vars_s["dwin_to_hid"])
 plt.figure()
-plt.plot(rec_vars_n["hid_to_outw"])
+plt.plot(rec_vars_s["whid_to_out"])
 plt.figure()
-plt.plot(rec_vars_n["hid_to_outdw"])
+plt.plot(rec_vars_s["dwhid_to_out"])
 
 plt.show()
-
-
-
--- simulator.py
+++ simulator.py
@@ -55,8 +55,8 @@ p["ETA_DECAY"]= 0.95      # UNUSED - presumably a decay in learning rate
 # spike recording
 p["SPK_REC_STEPS"]= int(p["TRIAL_MS"]/p["DT_MS"])
 p["REC_SPIKES"] = []
-p["REC_NEURONS"] = {}
-p["REC_SYNAPSES"] = {}
+p["REC_NEURONS"] = []
+p["REC_SYNAPSES"] = []
 p["WRITE_TO_DISK"]= True
 
 # ----------------------------------------------------------------------------
@@ -319,13 +319,12 @@ def run_yingyang(p):
         spike_ID[pop]= []
 
     rec_vars_n= {}
-    for pop, var in p["REC_NEURONS"].items():
+    for pop, var in p["REC_NEURONS"]:
         rec_vars_n[var+pop]= []
-
     rec_vars_s= {}
-    for pop, var in p["REC_SYNAPSES"].items():
+    for pop, var in p["REC_SYNAPSES"]:
         rec_vars_s[var+pop]= []
-        
+    print(rec_vars_s)        
     for trial in range(N_trial):
         if p["TRAIN"]:
             output.set_extra_global_param("label", int(Y_train[trial]))
@@ -344,15 +343,15 @@ def run_yingyang(p):
                         spike_t[pop].append(the_pop.spike_recording_data[0][0])
                         spike_ID[pop].append(the_pop.spike_recording_data[0][1])
 
-            for pop, var in p["REC_NEURONS"].items():
+            for pop, var in p["REC_NEURONS"]:
                 the_pop= model.neuron_populations[pop]
-                the_pop.pull_var_from_device[var]
+                the_pop.pull_var_from_device(var)
                 rec_vars_n[var+pop].append(the_pop.vars[var].view.copy())
 
-            for pop, var in p["REC_SYNAPSES"].items():
+            for pop, var in p["REC_SYNAPSES"]:
                 the_pop= model.synapse_populations[pop]
-                the_pop.pull_var_from_device[var]
-                rec_vars_n[var+pop].append(the_pop.vars[var].view.copy())
+                the_pop.pull_var_from_device(var)
+                rec_vars_s[var+pop].append(the_pop.vars[var].view.copy())
                                            
         if p["TRAIN"]:
             update_adam(learning_rate, adam_step, optimisers)
@@ -373,22 +372,22 @@ def run_yingyang(p):
         spike_t[pop]= np.hstack(spike_t[pop])
         spike_ID[pop]= np.hstack(spike_ID[pop])
 
-    for pop, var in p["REC_NEURONS"].items():
-        rec_var_n[var+pop]= np.vstack[rec_var_n[var+pop]]
+    for pop, var in p["REC_NEURONS"]:
+        rec_vars_n[var+pop]= np.vstack(rec_vars_n[var+pop])
         
-    for pop, var in p["REC_SYNAPSES"].items():
-        rec_var_s[var+pop]= np.vstack[rec_var_s[var+pop]]
+    for pop, var in p["REC_SYNAPSES"]:
+        rec_vars_s[var+pop]= np.vstack(rec_vars_s[var+pop])
         
     if p["WRITE_TO_DISK"]:            # Saving results
         for pop in p["REC_SPIKES"]:
             np.save(p["OUT_DIR"]+"/"+pop+"_spike_t", spike_t[pop])
             np.save(p["OUT_DIR"]+"/"+pop+"_spike_ID", spike_ID[pop])
 
-        for pop, var in p["REC_NEURONS"].items():
+        for pop, var in p["REC_NEURONS"]:
             np.save(p["OUT_DIR"]+"/"+var+pop, rec_vars_n[var+pop])
 
-        for pop, var in p["REC_SYNAPSES"].items():
-            np.save(p["OUT_DIR"]+"/"+var+pop, rec_vars_n[var+pop])
+        for pop, var in p["REC_SYNAPSES"]:
+            np.save(p["OUT_DIR"]+"/"+var+pop, rec_vars_s[var+pop])
             
     if not p["TRAIN"]:
         print("Correct: {}".format(good/(N_trial*p["N_BATCH"])))
