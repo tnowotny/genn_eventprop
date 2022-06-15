10db37341731a74d4476e049b2386c9f84a8866c
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2022-06-15 19:24:29 +0000

    Made changes to the recording system to allow more fine-grained control, allowing to peek at particular epochs/trials.

--- train_SHD.py
+++ train_SHD.py
@@ -7,7 +7,7 @@ p["TRIAL_MS"]= 1400
 p["DATASET"]= "SHD"
 p["TRAIN_DATA_SEED"]= 372
 p["TEST_DATA_SEED"]= 814
-p["NAME"]= "test31"
+p["NAME"]= "test32"
 p["NUM_HIDDEN"]= 256
 p["N_MAX_SPIKE"]= 1500
 p["DT_MS"]= 1
@@ -17,12 +17,12 @@ p["ADAM_BETA2"]= 0.999
 p["DEBUG"]= False
 p["DEBUG_HIDDEN_N"]= True
 p["LOAD_LAST"]= False
-p["N_EPOCH"]= 400
+p["N_EPOCH"]= 800
 p["N_BATCH"]= 256
 p["SUPER_BATCH"]= 1
 p["N_TRAIN"]= 7900 #20*p["N_BATCH"] #7756 
 p["N_VALIDATE"]= 512 # 256 # p["N_BATCH"] 
-p["ETA"]= 5e-3 #5e-3
+p["ETA"]= 1e-2 #5e-3
 p["SHUFFLE"]= True
 p["INPUT_HIDDEN_MEAN"]= 0.02
 p["INPUT_HIDDEN_STD"]= 0.01
@@ -32,7 +32,7 @@ p["W_REPORT_INTERVAL"] = 11000  # this should be at the end of the epoch (at fir
 p["TAU_MEM"] = 20.0
 p["TAU_SYN"] = 5.0
 p["REG_TYPE"]= "simple"
-p["LBD_UPPER"]= 5e-11 # keep in mind that the term is applied to all contributing spikes ...
+p["LBD_UPPER"]= 5e-12 # 5e-12 keep in mind that the term is applied to all contributing spikes ...
 p["LBD_LOWER"]= 1e-5
 p["NU_UPPER"]= 10 #*p["N_BATCH"]
 p["NU_LOWER"]= 5
@@ -58,6 +58,14 @@ if p["DEBUG"]:
     p["REC_SPIKES"]= ["input", "hidden"]
     #p["REC_NEURONS"]= [("output", "V"), ("output", "lambda_V"), ("output", "lambda_I")]
     #p["REC_SYNAPSES"]= [("hid_to_out", "w")]
+
+p["REC_SPIKES"]= ["input","hidden"]
+p["REC_SPIKES_EPOCH_TRIAL"]= [(0,0), (100,0), (799,0)]
+
+p["REC_NEURONS"]= [("output","V")]
+p["REC_NEURONS_EPOCH_TRIAL"]= [(0,0), (100,0), (799,0)]
+
+p["W_OUTPUT_EPOCH_TRIAL"]= [(0,0), (100,0), (799,0)]
 
 with open(os.path.join(p["OUT_DIR"], p["NAME"]+'.json'), 'w') as file:
     json.dump(p, file)
