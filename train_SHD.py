1592d6abf8d42fcc36c0fd3f97380d7868f9207e
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2022-10-13 17:23:55 +0000

    iStarting to clean up. This commit last one before removing unsused functions and commented parameter reminders.

--- train_SHD.py
+++ train_SHD.py
@@ -2,6 +2,7 @@ from simulator_SHD import *
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+from os.path import exists
 
 p["TRIAL_MS"]= 1400
 p["DATASET"]= "SHD"
@@ -9,7 +10,7 @@ p["TRAIN_DATA_SEED"]= 372
 p["TEST_DATA_SEED"]= 814
 p["MODEL_SEED"]= 135
 p["NAME"]= "test102"
-p["NUM_HIDDEN"]= 256
+p["NUM_HIDDEN"]= 128
 p["N_MAX_SPIKE"]= 1500
 p["DT_MS"]= 1
 p["PDROP_INPUT"]= 0.1
@@ -28,15 +29,15 @@ p["ETA"]= 1e-3 #1e-3 1e-4 # 5e-3
 p["SHUFFLE"]= True
 p["INPUT_HIDDEN_MEAN"]= 0.02 # 0.02
 p["INPUT_HIDDEN_STD"]= 0.01 # 0.01
-p["HIDDEN_OUTPUT_MEAN"]= 1.2 # 0.06 0.0
-p["HIDDEN_OUTPUT_STD"]= 0.6 # 0.03 0.3
+p["HIDDEN_OUTPUT_MEAN"]= 0.0 # 0.06 0.0
+p["HIDDEN_OUTPUT_STD"]= 0.3 # 0.03 0.3
 p["W_REPORT_INTERVAL"] = 11000  # this should be at the end of the epoch (at first trial of evaluation)
 p["TAU_MEM"] = 20.0 #20
-p["TAU_MEM_OUTPUT"]= 1000.0
+p["TAU_MEM_OUTPUT"]= 20.0
 p["TAU_SYN"] = 5.0 #5
 p["REG_TYPE"]= "simple"
-p["LBD_UPPER"]= 1e-8 # 2e-9 # 2e-8 # 2e-14 (since removal of N_Batch), 5e-12 keep in mind that the term is applied to all contributing spikes ...
-p["LBD_LOWER"]= 1e-8 #2e-8
+p["LBD_UPPER"]= 2e-9 # 2e-9 # 2e-8 # 2e-14 (since removal of N_Batch), 5e-12 keep in mind that the term is applied to all contributing spikes ...
+p["LBD_LOWER"]= 2e-9 #2e-8
 p["NU_UPPER"]= 14
 p["NU_LOWER"]= 5
 p["RHO_UPPER"]= 10000.0100
@@ -61,7 +62,7 @@ p["TAU_0"]= 2
 p["TAU_1"]= 25.0 #6.4
 p["ALPHA"]= 1e-3 #3e-3
 
-p["LOSS_TYPE"]= "first_spike_exp"
+p["LOSS_TYPE"]= "sum_weigh_exp"
 #p["LOSS_TYPE"]= "max"
 #p["LOSS_TYPE"]= "sum_weigh_linear"
 #p["LOSS_TYPE"]= "avg_xentropy"
@@ -69,16 +70,16 @@ p["LOSS_TYPE"]= "first_spike_exp"
 #p["AUGMENTATION"]= {}
 
 p["AUGMENTATION"]= {
-    "random_shift": 20.0,
+    "random_shift": 40.0,
 }
 
 #p["REDUCED_CLASSES"]= [0]
 
-p["SPEAKER_LEFT"]= 0
+p["SPEAKER_LEFT"]= 11
 p["COLLECT_CONFUSION"]= True
 p["TAU_ACCUMULATOR"]= 5.0
 
-p["HIDDEN_NOISE"]= 0.001
+p["HIDDEN_NOISE"]= 0.002
 
 """
 p["REC_NEURONS"]= [("output","avgInback"),("accumulator","V")]
@@ -117,34 +118,31 @@ with open(os.path.join(p["OUT_DIR"], p["NAME"]+'.json'), 'w') as file:
     json.dump(p, file)
     
 mn= SHD_model(p)
-spike_t, spike_ID, rec_vars_n, rec_vars_s,correct,correct_eval= mn.train(p)
-#spike_t, spike_ID, rec_vars_n, rec_vars_s,correct,correct_eval= mn.cross_validate_SHD(p)
+
+interrupt= False
+try:
+    spike_t, spike_ID, rec_vars_n, rec_vars_s,correct,correct_eval= mn.train(p)
+    #spike_t, spike_ID, rec_vars_n, rec_vars_s,correct,correct_eval= mn.cross_validate_SHD(p)
+except KeyboardInterrupt:
+    interrupt= True
+
+found= False
+i= -1
+while not found:
+    i=i+1
+    jname= p["NAME"]+'.'+str(i)+'.json'
+    found= not exists(jname)
+
+jfile= open(jname,'w')
+json.dump(p,jfile)
+rname= p["NAME"]+'.'+str(i)+'.summary.txt'
+sumfile= open(rname,'w')
+if interrupt:
+    label= 'Cancelled: '
+else:
+    label= 'Complete: '
+    
+sumfile.write("{}Training correct: {}, Valuation correct: {}".format(label,correct,correct_eval))
 
 print("correct: {}".format(correct))
 print("correct_eval: {}".format(correct_eval))
-if p["DEBUG"]:
-    plt.figure()
-    plt.scatter(spike_t["input"],700-spike_ID["input"],s=0.5)
-    plt.figure()
-    plt.scatter(spike_t["hidden"],spike_ID["hidden"],s=0.5)
-    #tmax= np.max(spike_t["hidden"])
-    #t= np.arange(0,tmax,p["TRIAL_MS"])
-    #v= np.vstack([ np.zeros(1,lent(t)), np.ones(1,len(t))])
-    #t= np.reshape(t,(1,len(t)))
-    #t= np.vstack([ t, t ])
-    #plt.plot(t,v, lw=0.1)
-    #plt.figure()
-    #plt.plot(rec_vars_n["Voutput"])
-    #plt.figure()
-    #plt.plot(rec_vars_n["lambda_Voutput"])
-    #plt.figure()
-    #plt.plot(rec_vars_n["lambda_Ioutput"])
-    #print(mn.output.extra_global_params["label"].view[0:20*p["N_BATCH"]:p["N_BATCH"]])
-    #plt.figure()
-    #h, x= np.histogram(spike_ID["hidden"],p["NUM_HIDDEN"])
-    #h= np.sort(h)
-    #plt.bar(np.arange(len(h)),np.log(h+2))
-#mn.hid_to_out.pull_var_from_device("w")
-#plt.figure()
-#plt.hist(mn.hid_to_out.vars["w"].view[:],100)
-plt.show()
