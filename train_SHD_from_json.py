9dc2cb8987782a3e68d3876dc2d0068201082d2c
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2022-10-14 15:20:21 +0000

    Digesting results and consolidating towards publication.

--- train_SHD_from_json.py
+++ train_SHD_from_json.py
@@ -2,9 +2,19 @@ from simulator_SHD import *
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+import sys
+
+if len(sys.argv) != 3:
+    print("usage: python train_SHD_from_json.py XXX.json NAME")
+    exit(1)
 
 with open(sys.argv[1],"r") as f:
-    p= json.load(f)
+    p0= json.load(f)
+
+for (name,value) in p0.items():
+    p[name]= value
+
+p["NAME"]= sys.argv[2]
 
 mn= SHD_model(p)
 spike_t, spike_ID, rec_vars_n, rec_vars_s,correct,correct_eval= mn.train(p)
