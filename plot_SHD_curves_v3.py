28c3c2fbe0f091f03d45e116da17e9e76d92b96a
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2022-11-30 19:22:21 +0000

    replaced old curve plotting script with correct one.

--- plot_SHD_curves_v3.py
+++ plot_SHD_curves_v3.py
@@ -1,37 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import json
+import sys
 
-"""
-i == 0, k == 0 - sum ffwd: scan_JADE_26
-i == 0, k == 1 - sum recur: 
-"""
+if len(sys.argv) < 2:
+    print("usage: python plot_SHD_curves_v3.py XXX.json (summary of get_best**)")
+    exit(1)
 
-#data_name= "scan_JADE_26/J26_scan_"
-data_name= "scan_JADE_27/J27_scan_"
-j= 1
-l= 0
-m= 0
-n= 0
-n2= 0
-n3= 1
-o= 0
+fname= sys.argv[1]
+with open(fname,"r") as f:
+    best= json.load(f)
 
 ptrain= []
 ltrain= []
 ptest= []
 ltest= []
 
-N= 7
+N= 10
 N_epoch= 300
 
-for i in range(4):
-    for k in range(2):
-        scan_no= ((i*2+j)*2+k)*2+m
-        if i == 3 or i == 0:
-            scan_no+= 1 # consider the slightly stronger regularisation
-        print(scan_no)
-        jname= str(scan_no)+"_results.txt"
-        d= np.loadtxt(data_name+jname)
+for b in best:
+    with open(b["path2"]+str(b["id2"])+"_results.txt", "r") as f:
+        d= np.loadtxt(f)
         print(d.shape)
         d= d[:N_epoch*N,:]
         ptrain.append(d[:,1].reshape((N,N_epoch)).T)
@@ -39,38 +29,48 @@ for i in range(4):
         ptest.append(d[:,3].reshape((N,N_epoch)).T)
         ltest.append(d[:,4].reshape((N,N_epoch)).T)
 
-#idx= [ 0, 4, 1, 5, 2, 6, 3, 7 ]
+idx= [ 0, 4, 1, 5, 2, 6, 3, 7 ]
 
-#print(perf)
+ptrain= [ ptrain[idx[i]] for i in range(8) ]
+ptest= [ ptest[idx[i]] for i in range(8) ]
+ltrain= [ ltrain[idx[i]] for i in range(8) ]
+ltest= [ ltest[idx[i]] for i in range(8) ]
 
-#ptrain= [ ptrain[idx[i]] for i in range(8) ]
-#ptest= [ ptest[idx[i]] for i in range(8) ]
-#ltrain= [ ltrain[idx[i]] for i in range(8) ]
-#ltest= [ ltest[idx[i]] for i in range(8) ]
-
-plt.figure()
+plt.figure(figsize= (8,4.5))
 for p in ptrain:
-    plt.plot(np.mean(p,axis=1))
+    m= np.mean(p,axis=1)
+    plt.plot(m)
+    s= np.std(p,axis=1)
+    plt.fill_between(range(N_epoch), m-s, m+s,alpha=0.4)
+    plt.xlim([ 0, 400 ])
     plt.legend(["sum of V \n (ffwd)", "sum of V \n (recur)", "sum_exp \n of V (ffwd)", "sum_exp \n of V (recur)", "time of first \n spike (ffwd)", "time of first \n spike (recur)", "max of V \n (ffwd)","max of V \n (recur)" ])
+    plt.ylim([ 0, 1.05 ])
     plt.title("training performance")
-
-plt.figure()
+    plt.xlabel("epoch")
+    plt.ylabel("fraction correct")
+    plt.savefig("best_training_curve.png",dpi=300)
+    
+plt.figure(figsize= (6,4.5))
 for p in ptest:
-    plt.plot(np.mean(p,axis=1))
-    plt.legend(["sum of V \n (ffwd)", "sum of V \n (recur)", "sum_exp \n of V (ffwd)", "sum_exp \n of V (recur)", "time of first \n spike (ffwd)", "time of first \n spike (recur)", "max of V \n (ffwd)","max of V \n (recur)" ])
-    plt.title("testing preformance")
+    m= np.mean(p,axis=1)
+    plt.plot(m)
+    s= np.std(p,axis=1)
+    plt.fill_between(range(N_epoch), m-s, m+s,alpha=0.4)    
+    plt.ylim([ 0, 1.05 ])
+    plt.title("testing performance")
+    plt.xlabel("epoch")
+    plt.ylabel("fraction correct")
+    plt.savefig("best_testing_curve.png",dpi=300)
 
 plt.figure()
 for p in ltrain:
     plt.plot(np.mean(p,axis=1))
-    plt.ylim([ 0, 1.05 ]
     plt.legend(["sum of V \n (ffwd)", "sum of V \n (recur)", "sum_exp \n of V (ffwd)", "sum_exp \n of V (recur)", "time of first \n spike (ffwd)", "time of first \n spike (recur)", "max of V \n (ffwd)","max of V \n (recur)" ])
     plt.title("training loss")
 
 plt.figure()
 for p in ltest:
     plt.plot(np.mean(p,axis=1))
-    plt.ylim([ 0, 1.05 ]
     plt.legend(["sum of V \n (ffwd)", "sum of V \n (recur)", "sum_exp \n of V (ffwd)", "sum_exp \n of V (recur)", "time of first \n spike (ffwd)", "time of first \n spike (recur)", "max of V \n (ffwd)","max of V \n (recur)" ])
     plt.title("testing loss")
 
