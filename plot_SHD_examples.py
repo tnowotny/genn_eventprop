a32537613b54e4ac10fe5a63afa0a52d75bad130
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2023-07-06 19:49:11 +0000

    reactivated using tonic for data loading

--- plot_SHD_examples.py
+++ plot_SHD_examples.py
@@ -6,8 +6,9 @@ import sys
 
 p["DATASET"]= "SHD"
 p["RESCALE_X"]= 1.0
-p["RESCALE_T"]= 0.1
-p["TRIAL_MS"]= 100
+p["RESCALE_T"]= 1.0
+p["TRIAL_MS"]= 1000
+p["DT_MS"]= 8
 mn= SHD_model(p)
 mn.plot_examples([0,1,2,3,6,7,8,9,10,11],int(sys.argv[1]),10,"train")
 
